837130644af704e78e74bd895bdf6df0929b39c9
Andrew Boltachev <andrewboltachev@gmail.com>
2015-01-18 00:35:16 +0300

    structs

--- sbnf.c
+++ sbnf.c
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+
+enum AutomatonKind{AKChar};
+
+typedef enum AutomatonKind AutomatonKindEnum;
+
+struct AutomatonStruct {
+    AutomatonKindEnum kind;
+    char * name;
+};
+
+typedef struct AutomatonStruct Automaton;
+
+Automaton * Char(const char * value, const char * name) {
+    Automaton * st = malloc(sizeof(Automaton));
+    st->kind = AKChar;
+    strcpy(st->name, name);
+    return st;
+}
+
 int foo(int a, int b) {
     return 4;
 }
