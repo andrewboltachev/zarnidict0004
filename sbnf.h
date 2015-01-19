03287674241b1f65f93ff0109ce6e4914d0bff68
Andrew Boltachev <andrewboltachev@gmail.com>
2015-01-19 22:29:47 +0300

    h

--- sbnf.h
+++ sbnf.h
@@ -1,1 +1,86 @@
-int foo(int, int);
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+typedef enum {AKChar} AutomatonKind;
+
+typedef struct {
+    AutomatonKind kind;
+    char * name;
+    wchar_t * value;
+} Automaton;
+
+typedef struct {
+    wchar_t * value;
+    void * payload;
+} InputChar;
+
+typedef struct {
+    InputChar ** input_chars;
+    int count;
+} Iterator;
+
+InputChar * ic(wchar_t * value, void * payload) {
+    InputChar * ichar = malloc(sizeof(InputChar));
+    ichar->value = value;
+    ichar->payload = payload;
+    return ichar;
+}
+
+Automaton * Char(wchar_t * value, char * name) {
+    Automaton * st = malloc(sizeof(Automaton));
+    st->kind = AKChar;
+    st->name = strdup(name);
+    st->value = wcsdup(value);
+    return st;
+}
+
+
+typedef union {
+    InputChar *input_char;
+} AutomatonResult;
+
+
+int input_cmp(AutomatonResult * a, AutomatonResult * b) {
+    return (a == b);
+};
+
+AutomatonResult * run(Automaton * a, Iterator * it) {
+
+}
+
+int foo(int a, int b) {
+    return 4;
+}
+
+typedef union NodeContent {
+    struct Node * node;
+    InputChar * input_char;
+} NodeContent;
+
+typedef struct Node {
+    NodeContent * content;
+    char * name;
+    char * klass;
+} Node;
+
+
+Node * node() {
+    Node * n = malloc(sizeof(Node));
+    return n;
+}
+
+int node_cmp(Node *a, Node *b) {
+    return 0;
+}
+
+Node * node_ic(wchar_t * c) {
+    InputChar * input_char = malloc(sizeof(InputChar));
+    input_char->value = c;
+    input_char->payload = NULL;
+    NodeContent * node_content = malloc(sizeof(NodeContent));
+    node_content->input_char = input_char;
+    Node * node = malloc(sizeof(Node));
+    node->content = node_content;
+    return node;
+}
--- test_sbnf.c
+++ test_sbnf.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include "sbnf.h"
@@ -6,11 +7,37 @@
     printf("%s\n", #fn_name);\
     fn_name();
 
-void test_foo() {
-    assert(4 == foo(2, 2));
+void test_input_cmp0001() {
+    InputChar * a = ic(L"a", NULL);
+    InputChar * b = ic(L"b", NULL);
+    NodeContent * ca = malloc(sizeof(NodeContent));
+    ca->input_char = a;
+    NodeContent * cb = malloc(sizeof(NodeContent));
+    cb->input_char = b;
+    Node * na = malloc(sizeof(Node));
+    Node * nb = malloc(sizeof(Node));
+    assert(1 != node_cmp(na, nb));
+}
+
+void test_input_cmp0002() {
+    //InputChar * a = ic(L"a", NULL);
+    //InputChar * a = ic(L"a", NULL);
+    //assert(1 != node_cmp(na, nb));
+}
+
+void test_Char() {
+    char * v = "aaa";
+    Automaton * g = Char(L"ӹ", v);
+    /*InputChar * x = {
+        ic(L"", NULL)
+    };
+    AutomatonResult * r = run(g, x);
+    assert(4 == foo(2, 2));*/
 }
 
 int main() {
-    run_test(test_foo);
+    run_test(test_input_cmp0001);
+    run_test(test_input_cmp0002);
+    //run_test(test_Char);
     return 0;
 }
